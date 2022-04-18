77ce2f98c4687e8028ef161824eef61e50823264
Vilhelms <vilhelmsbavins123@gmail.com>
2022-04-18 18:34:09 +0300

    Base stable

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Terminal Color Scheme
-cp /etc/bash.bashrc /etc/bash.bashrc.backup #sudo
+cp /etc/bash.bashrc /etc/bash.bashrc.backup #sudo needed
 
-mv bash.bashrc /etc/bash.bashrc #sudo
+mv bash.bashrc /etc/bash.bashrc #sudo needed
 
-mv DIR_COLORS /etc/ #sudo
+mv DIR_COLORS /etc/ #sudo needed
 
 mv .bashrc /.bashrc
 
@@ -15,6 +15,6 @@ cp /etc/pacman.conf /etc/pacman.conf.backup
 sed -i 's/#Color/Color/g' /etc/pacman.conf
 
 #Syntax Highlighting in Nano
-cp /etc/nanorc /etc/nanorc.backup #sudo
+cp /etc/nanorc /etc/nanorc.backup #sudo needed
 
-echo include "/usr/share/nano/*.nanorc" >> /etc/nanorc #sudo
+echo include "/usr/share/nano/*.nanorc" >> /etc/nanorc #sudo needed
